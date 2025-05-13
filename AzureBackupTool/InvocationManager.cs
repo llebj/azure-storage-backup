9a92476cf7d39a360c2914bcba2e6969249b2696
Joe Bell <35937399+llebj@users.noreply.github.com>
2025-05-13 20:14:32 +0100

    InvocationManager handles duplicate Invocations.

--- AzureBackupTool/InvocationManager.cs
+++ AzureBackupTool/InvocationManager.cs
@@ -2,19 +2,40 @@ namespace AzureBackupTool;
 
 public class InvocationManager
 {
+    private readonly Dictionary<string, HashSet<DateTimeOffset>> _dictionary = [];
+
     private readonly PriorityQueue<Invocation, DateTimeOffset> _queue = new();
 
     public void AddInvocation(Invocation invocation)
     {
+        if (_dictionary.TryGetValue(invocation.ProfileId, out var invocationTimes) 
+            && invocationTimes.Contains(invocation.InvokeAt))
+        {
+            return;
+        }
+
+        if (!_dictionary.TryGetValue(invocation.ProfileId, out HashSet<DateTimeOffset>? value))
+        {
+            value = [];
+            _dictionary.Add(invocation.ProfileId, value);
+        }
+        value.Add(invocation.InvokeAt);
+
         _queue.Enqueue(invocation, invocation.InvokeAt);
     }
 
     public List<Invocation> GetPendingInvocations(DateTimeOffset time)
     {
         List<Invocation> result = [];
-        while (_queue.Peek().InvokeAt <= time)
+        while (_queue.TryPeek(out Invocation invocation, out _) && invocation.InvokeAt <= time)
         {
             result.Add(_queue.Dequeue());
+            _dictionary[invocation.ProfileId].Remove(invocation.InvokeAt);
+
+            if (_dictionary[invocation.ProfileId].Count == 0)
+            {
+                _dictionary.Remove(invocation.ProfileId);
+            }
         }
         return result;
     }
--- AzureBackupToolTests/InvocationManagerTests.cs
+++ AzureBackupToolTests/InvocationManagerTests.cs
@@ -6,20 +6,113 @@ public class InvocationManagerTests
 {
     public class GivenAnInvocationManagerWithASingleInvocation
     {
-        private readonly Invocation _invocation = new("test-profile", DateTimeOffset.UtcNow);
-
-        [Fact]
-        public void WhenTheTimeIsBeforeTheInvocation_ThenReturnNothing()
+        public class WhenTheTimeIsBeforeTheInvocation
         {
-            // Arrange
-            InvocationManager invocationManager = new();
-            invocationManager.AddInvocation(_invocation);
+            [Fact]
+            public void ThenReturnNothing()
+            {
+                // Arrange
 
-            // Act
-            var invocations = invocationManager.GetPendingInvocations(_invocation.InvokeAt.AddHours(1));
+                Invocation invocation = new("test-profile", DateTimeOffset.UtcNow);
+                InvocationManager invocationManager = new();
+                invocationManager.AddInvocation(invocation);
 
-            // Assert
-            Assert.Empty(invocations);
+                // Act
+                var invocations = invocationManager.GetPendingInvocations(invocation.InvokeAt.AddHours(-1));
+
+                // Assert
+                Assert.Empty(invocations);
+            }
+        }
+
+        public class WhenTheTimeIsAfterTheInvocation
+        {
+            [Fact]
+            public void ThenReturnTheInvocation()
+            {
+                // Arrange
+                Invocation invocation = new("test-profile", DateTimeOffset.UtcNow);
+                InvocationManager invocationManager = new();
+                invocationManager.AddInvocation(invocation);
+
+                // Act
+                var invocations = invocationManager.GetPendingInvocations(invocation.InvokeAt.AddHours(1));
+
+                // Assert
+                Assert.Single(invocations);
+            }
+
+            [Fact]
+            public void ThenTheManagerDoesNotServeTheSameInvocationTwice()
+            {
+                // Arrange
+                Invocation invocation = new("test-profile", DateTimeOffset.UtcNow);
+                InvocationManager invocationManager = new();
+                invocationManager.AddInvocation(invocation);
+
+                // Act
+                var time = invocation.InvokeAt.AddHours(1);
+                _ = invocationManager.GetPendingInvocations(time);
+                var invocations = invocationManager.GetPendingInvocations(time);
+
+                // Assert
+                Assert.Empty(invocations);
+            }
+
+            [Fact]
+            public void ThenASecondIdenticalInstanceCanBeAddedAndServedAfterTheFirstInstance()
+            {
+                Invocation invocation = new("test-profile", DateTimeOffset.UtcNow);
+                InvocationManager invocationManager = new();
+                invocationManager.AddInvocation(invocation);
+                _ = invocationManager.GetPendingInvocations(invocation.InvokeAt.AddHours(1));
+
+                // Act
+                invocationManager.AddInvocation(invocation);
+                var invocations = invocationManager.GetPendingInvocations(invocation.InvokeAt.AddHours(1));
+
+                // Assert
+                Assert.Single(invocations);
+            }
+        }
+
+        public class WhenADuplicateInvocationIsAdded
+        {
+            [Fact]
+            public void ThenOnlyASingleInvocationInstanceIsReturned()
+            {
+                // Arrange
+                Invocation invocation = new("test-profile", DateTimeOffset.UtcNow);
+                InvocationManager invocationManager = new();
+                invocationManager.AddInvocation(invocation);
+                invocationManager.AddInvocation(invocation);
+
+                // Act
+                var invocations = invocationManager.GetPendingInvocations(invocation.InvokeAt.AddHours(1));
+
+                // Assert
+                Assert.Single(invocations);
+            }
+        }
+    
+        public class WhenAnInvocationIsAddedForADifferentProfile
+        {
+            [Fact]
+            public void ThenBothInvocationsCanBeRetrieved()
+            {
+                // Arrange
+                Invocation invocation = new("profile-one", DateTimeOffset.UtcNow);
+                InvocationManager invocationManager = new();
+                invocationManager.AddInvocation(invocation);
+
+                // Act
+                Invocation invocation2 = new("profile-two", invocation.InvokeAt);
+                invocationManager.AddInvocation(invocation2);
+                var invocations = invocationManager.GetPendingInvocations(invocation.InvokeAt.AddHours(1));
+
+                // Assert
+                Assert.Equal(2, invocations.Count);
+            }
         }
     }
 }
