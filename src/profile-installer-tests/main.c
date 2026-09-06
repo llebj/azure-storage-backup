fbfe698eae8eb2ae2a9665a3f3e2c9bc614454a9
Joe Bell <35937399+llebj@users.noreply.github.com>
2026-09-06 07:43:26 +0000

    Created Makefile for profile-installer-tests.

--- src/profile-installer-tests/main.c
+++ src/profile-installer-tests/main.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 #include "vendor/unity.h"
 #include "vendor/unity_internals.h"
 
@@ -11,8 +13,18 @@ void tearDown(void)
 {
 }
 
+void test_test()
+{
+	size_t count = 0;
+	bool result = count_profiles(&count, NULL, 0);
+	TEST_ASSERT_TRUE(result);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
+
+	RUN_TEST(test_test);
+
 	return UNITY_END();
 }
